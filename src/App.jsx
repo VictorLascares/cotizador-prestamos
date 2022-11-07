53ece206f0e9976b941c7d5378bd61e36c0995f2
VictorLascares <vmlascares@gmail.com>
2022-11-07 12:32:30 -0600

    Agregando un state para ek plazo a pagar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { formatMoney } from "./helpers";
 
 function App() {
   const [amount, setAmount] = useState(10000);
+  const [term, setTerm] = useState("6");
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -52,6 +53,20 @@ function App() {
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatMoney(amount)}
       </p>
+
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className="text-indigo-600">plazo</span> a pagar
+      </h2>
+
+      <select 
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500" 
+        value={term}
+        onChange={ e => setTerm(e.target.value) }
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
     </div>
   );
 }
