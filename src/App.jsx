941940e0ab37e67fcfd60b65ea18d937cb48318b
VictorLascares <vmlascares@gmail.com>
2022-11-07 08:37:20 -0600

    Agregando funcionalidad para cambiar el state de cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react"
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [amount, setAmount] = useState(10000);
-  console.log(amount);
+
+  function handleChange(e) {
+    setAmount(e.target.value)
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
-      <input 
+      <input
         type="range"
         className="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={ handleChange }
       />
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{ amount }</p>
     </div>
- )
+  );
 }
 
-export default App
+export default App;
