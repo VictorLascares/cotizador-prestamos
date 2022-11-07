f03384355b9429e5c6dc9b378f53aa87974226c5
VictorLascares <vmlascares@gmail.com>
2022-11-07 08:19:26 -0600

    Agregando input para cantidades

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+      <input 
+        type="range"
+        className="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
     </div>
  )
 }
