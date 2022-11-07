5e1094b9104d88651fbf5dc3daee39597b397b1f
VictorLascares <vmlascares@gmail.com>
2022-11-07 09:13:28 -0600

    Agregando botones para incremento y decremento

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,60 @@ function App() {
   const STEP = 100;
 
   function handleChange(e) {
-    setAmount(e.target.value)
+    setAmount(Number(e.target.value));
+  }
+
+  function handleClickDecrement() {
+    const value = amount - STEP;
+
+    if (value < MIN) {
+      alert("Cantidad no válida");
+      return;
+    }
+    setAmount(value);
+  }
+
+  function handleClickIncrease() {
+    const value = amount + STEP;
+    if (value > MAX) {
+      alert("Cantidad no válida");
+      return;
+    }
+    setAmount(value);
   }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <div className="flex justify-between my-14">
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500"
+          onClick={handleClickDecrement}
+        >
+          &#8722;
+        </button>
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500"
+          onClick={handleClickIncrease}
+        >
+          &#43;
+        </button>
+      </div>
       <input
         type="range"
         className="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
-        onChange={ handleChange }
+        onChange={handleChange}
         min={MIN}
         max={MAX}
         step={STEP}
         value={amount}
       />
-      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{ amount }</p>
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {amount}
+      </p>
     </div>
   );
 }
