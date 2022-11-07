d7ff0bf687f564ff966ea0b9f393e3dbd556a2f6
VictorLascares <vmlascares@gmail.com>
2022-11-06 22:10:45 -0600

    Creando el componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <h1 className="text-red-600">Hola Mundo</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+    </div>
  )
 }
 
