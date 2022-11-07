555191e7339b53353e159b715ce3c96de12882ea
VictorLascares <vmlascares@gmail.com>
2022-11-07 08:25:48 -0600

    Agregando state para la cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 import Header from "./components/Header"
 
 function App() {
+  const [amount, setAmount] = useState(10000);
+  console.log(amount);
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
